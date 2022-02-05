618a9a1404a393288b7ff0c5f5fbb701987d97f9
Bulat15 <mazikazik64@gmail.com>
2022-02-05 15:11:26 +0400

    lesson_8

---
+++ FED-1_12.01.2022/lesson_7/javaScript/style.js
@@ -0,0 +1,11 @@
+
+let value_1 = 12;
+let value_2 = 14;
+let value_3 = 14;
+
+if(value_1 === value_2 && value_1 === value_3){
+  console.log('все числа равны')
+}else if(value === 12 
+    && (value_2 === 12 || value_3 === 12 )){
+      
+    } 
